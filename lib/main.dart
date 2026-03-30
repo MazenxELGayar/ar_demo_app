c61a7a11245cb63463b6ebd894eba990a0f14418
MazenxELGayar <56363166+MazenxELGayar@users.noreply.github.com>
2026-03-30 09:46:13 +0200

    still base

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ARDemo());
+}
+
+
+class ARDemo extends StatelessWidget {
+  const ARDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello, AR!'),
+        ),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ar_demo/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ARDemo());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
